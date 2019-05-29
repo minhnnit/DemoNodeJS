66ef3729a684881ff7bf9eeb9732db6fd4fc2430
Nhu Minh <minhnnit>
2019-05-29 09:33:11 +0700

    edit demo aplly multi coupon at once in s artmoderns using NodeJS nightmareJS

--- demoNodeJS.js
+++ demoNodeJS.js
@@ -1,26 +1,24 @@
 const Nightmare = require('nightmare')
+const nightmare = Nightmare({show: true})
 
-main().catch(console.error)
-
-async function main() {
-	const nightmare = Nightmare({ show: true})
-	const CouponArr = [ 'CAINAYKHONGDUOC', 'CAINAYKHONGDUOCNOT', 'SPECIAL10', 'SPECIAL15' ]
-	
-	for(let i = 0; i < CouponArr.length; i++){
-		const c = CouponArr[i]
-		
-		const CouponApply = await nightmare
+async function gotoUrl(){
+	await nightmare
 		.viewport(1366,768)
 		.goto('https://www.smartmoderns.com')
-		.click('.grid-link__title')
-		.click('.grid-link__title')
+		.click('#portable-headphones-and-technology-for-the-active-lifestyle > main > div > div > div:nth-child(4) > div:nth-child(1) > a > p')
+		.click('#wireless-sport-headphones > main > div > div > div.grid-uniform.grid-link__container > div:nth-child(1) > a > p.grid-link__title')
 		.click('#AddToCart')
-		.wait(5000)
+		.wait('#vncvtw')
 		.click('button[name="checkout"]')
-		.wait(5000)
-		.type('#checkout_reduction_code',c)
+		.wait('#countdownhere')
+	
+}
+async function  typeCoupon(Coupon) {
+	await nightmare
+		.insert('#checkout_reduction_code',Coupon)
 		.click('#order-summary > div > div.order-summary__section.order-summary__section--discount > form:nth-child(3) > div > div > div > button')
 		.wait(5000)
+		.type('#checkout_reduction_code',null)
 		.evaluate(function() {
 			var obj = {};
 			obj.Price = document.querySelectorAll('.total-line__price .order-summary__emphasis')[0].textContent.replace("$","");
@@ -35,16 +33,23 @@ async function main() {
 		  
       }).then(function(rs) {
 		  if(rs.TotalPrice > 0){
-			  console.log(c + " : Code apply successfully" +"-"+ "Discount Amount :" + rs.TotalPrice);
+			  console.log(Coupon + " : Code apply successfully" +"-"+ "Discount Amount :" + rs.TotalPrice);
 		  }
 		  else if(!rs.valid) {
-			 console.log( c + ": Invalid code"); 
+			  console.log(Coupon + ": Invalid code"); 
 		  }
 		 
 	  });
+}
+
+async function main() {
+	const CouponArr = [ 'CAINAYKHONGDUOC', 'CAINAYKHONGDUOCNOT', 'SPECIAL10', 'SPECIAL15' ]
+	await gotoUrl();
+		for(let couponCode of CouponArr) {
+			await typeCoupon(couponCode)
+		}
+	await nightmare.end()
 	}
 	
-	await nightmare.end()
+	main();
 	
-	
-}
